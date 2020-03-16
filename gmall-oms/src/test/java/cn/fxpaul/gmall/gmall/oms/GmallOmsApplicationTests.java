196edd9248841fdad16c31a5e1d6c20ed023451d
FXPaUl-only <18786672393@163.com>
2020-03-17 00:05:11 +0800

    项目结构创建完成

---
+++ gmall-oms/src/test/java/cn/fxpaul/gmall/gmall/oms/GmallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fxpaul.gmall.gmall.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-sms/src/test/java/cn/fxpaul/gmall/gmall/sms/GmallSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fxpaul.gmall.gmall.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
