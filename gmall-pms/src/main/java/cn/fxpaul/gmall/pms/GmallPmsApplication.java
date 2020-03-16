cbfbd565caaecd52f10f64b162d141f80338347f
FXPaUl-only <18786672393@163.com>
2020-03-17 01:25:27 +0800

    commit

--- gmall-pms/src/main/java/cn/fxpaul/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/cn/fxpaul/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,16 @@
 package cn.fxpaul.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1. 配置整合  dubbo
+ * 2. 整合mp
+ * */
+@EnableDubbo
+@MapperScan(basePackages = "cn.fxpaul.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
--- gmall-pms/src/test/java/cn/fxpaul/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/cn/fxpaul/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,26 @@
 package cn.fxpaul.gmall.pms;
 
+import cn.fxpaul.gmall.pms.entity.Product;
+import cn.fxpaul.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    private ProductService productService;
+
+
+
+
     @Test
     void contextLoads() {
+        Product byId = productService.getById(1);
+        System.out.println(byId.getName());
+
+
     }
 
 }
